473f3b04440673f8d88ecb4598d1075c459e7121
Davy Landman <davy.landman@gmail.com>
2014-06-04 16:41:36 +0200

    Removed assumption of location and project name

--- src/org/rascalmpl/junit/runner/RascalFileLookup.java
+++ src/org/rascalmpl/junit/runner/RascalFileLookup.java
@@ -1,12 +1,20 @@
 package org.rascalmpl.junit.runner;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.jar.Manifest;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jdt.internal.junit.model.TestElement;
 import org.eclipse.jdt.internal.junit.model.TestRoot;
+import org.eclipse.jdt.junit.model.ITestRunSession;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbench;
@@ -53,7 +61,7 @@ public class RascalFileLookup {
 					String testName = name.group(1);
 					int offset = Integer.parseInt(name.group(2));
 					int length = Integer.parseInt(name.group(3));
-					openRascalTest(parent, testName, offset, length);
+					openRascalTest(test.getParentContainer().getTestRunSession(), parent, testName, offset, length);
 					//System.out.println("We have a rascal thing: " + parent +"::"+testName + "(" + lineNumber + ")");
 					return true;					
 				}
@@ -65,14 +73,14 @@ public class RascalFileLookup {
 				// this is root level contain which tends to be a folder
 				return false;
 			}
-			openRascalTest(test.getTestName(), "", 0, 0);
+			openRascalTest(test.getParentContainer().getTestRunSession(), test.getTestName(), "", 0, 0);
 			return true;
 		}
 		return false;
 	}
-	private static void openRascalTest(String parent, String name, int offset, int length) {
-		String file = parent.replace("::", "/").replace("\\","");
-		openFile("rascal", "/src/org/rascalmpl/library/" + file + ".rsc", offset, length);
+	private static void openRascalTest(ITestRunSession session, String parent, String name, int offset, int length) {
+		String file = parent.replace("::", "/").replace("\\","") + ".rsc";
+		openFile(session.getLaunchedProject().getProject(), file, offset, length);
 	}
 
 	private static IWorkbenchWindow getWorkbenchWindow() {
@@ -85,12 +93,23 @@ public class RascalFileLookup {
 		return win;
 	}
 	
-	private static void openFile(final String projectName, String path, final int offset,final int length) {
-		IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(projectName);
+	private static void openFile(final IProject project, String path, final int offset,final int length) {
 		if (project == null) {
 			return;
 		}
-		final FileEditorInput target = new FileEditorInput(project.getFile(path));
+		List<String> roots = tryFindRascalLibraryPaths(project);
+		if (roots.isEmpty()) {
+			return;
+		}
+		IFile fullFileName = null;
+		for (String r : roots) {
+			fullFileName = project.getFile(r + "/" + path);
+			if (fullFileName.exists()) {
+				break;
+			}
+		}
+
+		final FileEditorInput target = new FileEditorInput(fullFileName);
 		
 		final IWorkbenchWindow win = getWorkbenchWindow();
 		if (win != null) {
@@ -111,5 +130,49 @@ public class RascalFileLookup {
 				});
 			}
 		}
+	}
+	
+	private static List<String> tryFindRascalLibraryPaths(IProject project) {
+		try {
+			IFile rascalMF = project.getFile("/META-INF/RASCAL.MF");
+			if (rascalMF.exists()) {
+				return getAttributeList(rascalMF.getContents(), "Source", "/src/org/rascalmpl/library/");
+			}
+		} catch (CoreException e) {
+		}
+		List<String> result = new ArrayList<>();
+		result.add("/src/org/rascalmpl/library/");
+		return result;
+	}
+	
+	private static String[] trim(String[] elems) {
+		for (int i = 0; i < elems.length; i++) {
+			elems[i] = elems[i].trim();
+		}
+		return elems;
+	}
+
+	private static List<String> getAttributeList(InputStream mf, String label,
+			String def) {
+		if (mf != null) {
+			try {
+				Manifest manifest = new Manifest(mf);
+				String source = manifest.getMainAttributes().getValue(label);
+
+				if (source != null) {
+					return Arrays.<String> asList(trim(source.split(",")));
+				}
+			} catch (IOException e) {
+				// ignore
+			} finally {
+				try {
+					mf.close();
+				} catch (IOException e) {
+					// too bad
+				}
+			}
+		}
+
+		return Arrays.<String> asList(new String[] { def });
 	}
 }
