d57b8984994d5383acf3ce0a95a4727c2a21ebf0
Davy Landman <davy.landman@gmail.com>
2014-06-05 09:25:05 +0200

    Fixed license headers

--- src/org/rascalmpl/junit/runner/TestNameTranslator.java
+++ src/org/rascalmpl/junit/runner/TestNameTranslator.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2014-2014 CWI
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   * Davy Landman - CWI
+ *   
+*******************************************************************************/
 package org.rascalmpl.junit.runner;
 
 import java.io.IOException;
